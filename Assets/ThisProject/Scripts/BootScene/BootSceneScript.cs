1789cee0df62a54f55fcab069b4c52ecb4296bf5
licrosea <sevenblock426@gmail.com>
2021-12-11 12:14:30 +0900

    View系の枠組み追加 BootScene仮組　シングルトン用のオブジェクト作成

---
+++ Assets/ThisProject/Scripts/BootScene/BootSceneScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootSceneScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject singletonObject = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: 必要に応じてマネージャ等の初期化
+        DontDestroyOnLoad( singletonObject );
+
+        // TODO: 共通定義とかからシーン名引っ張ってシーンをロード
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
