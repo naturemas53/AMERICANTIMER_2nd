0e33d079a35b1784bc42959bfe0c073d9e830d5e
licrosea <sevenblock426@gmail.com>
2022-06-13 22:51:42 +0900

    データ系のやつらをなんとなーくstructに

--- Assets/ThisProject/Scripts/GameSystem/DataStructDefines.cs
+++ Assets/ThisProject/Scripts/GameSystem/DataStructDefines.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // プレイヤーの情報.
-public class PlayerData
+public struct PlayerData
 {
-    public string playerName = "";
-    public Color playerColor = new Color();
+    public string playerName;
+    public Color playerColor;
 }
 
 // 各ラウンドの情報.
-public class RoundData
+public struct RoundData
 {
-    public float RemainTime = 0.0f; // 設定した試合時間
-    public float Multiplayer = 0.0f; // 倍率
-    public float DuelTime = 0.0f; // ２人になってから試合した時間
-    public List<PlayerData> targetPlayers = null; // ペナルティを受ける人
-    public List<MissionData> appearMissions = null; // 発生したミッションのデータ
+    public float RemainTime; // 設定した試合時間
+    public float Multiplayer; // 倍率
+    public float DuelTime; // ２人になってから試合した時間
+    public List<PlayerData> targetPlayers; // ペナルティを受ける人
+    public List<MissionData> appearMissions; // 発生したミッションのデータ
 }
 
 // ミッション情報.
-public class MissionData
+public struct MissionData
 { 
-    public float AppearTime = 0.0f; // ミッション発生時の時間.
-    public string overViewText =""; // ミッションの簡単な概要.
+    public float AppearTime; // ミッション発生時の時間.
+    public float ApplyTime; // ミッション発生時間.
+    public string overViewText; // ミッションの簡単な概要.
 
     // HACK: もしかしたら、対象者については、
     //       結果に統合した方がいいかも...?
-    public List<string> targetPlayers = null; // 対象者.
+    public List<string> targetPlayers; // 対象者.
 
     // ミッションの結果 で結果情報と時間を取得できる何か
 }
