d3e5cb03d11b084fa14c59469eec7eb3d2a52405
licrosea <sevenblock426@gmail.com>
2022-06-13 22:59:27 +0900

    試合結果を保持するシングルトンを追加

---
+++ Assets/ThisProject/Scripts/GameSystem/RoundLogSet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundLogSet : SingletonMonoBehaviour<RoundLogSet>
+{
+    List<RoundData> roundDatas;
+
+    public List<RoundData> LogDatas
+    {
+        get
+        {
+            // なんかあった時ようにコピーで渡します.
+            return new List<RoundData>(roundDatas);
+        }
+    }
+
+    public int LogCount
+    {
+        get { return roundDatas.Count; }
+    }
+
+    protected override void Initialize()
+    {
+        roundDatas = new List<RoundData>();
+        roundDatas.Clear();
+
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// 試合結果を追加します.
+    /// </summary>
+    /// <param name="roundData"> 追加する試合結果 </param>
+    public void AddRoundLog(RoundData roundData)
+    {
+        roundDatas.Add( roundData );
+    }
+
+    /// <summary>
+    /// 試合結果をすべて消去します
+    /// </summary>
+    public void ClearLog()
+    {
+        roundDatas.Clear();
+    }
+}
