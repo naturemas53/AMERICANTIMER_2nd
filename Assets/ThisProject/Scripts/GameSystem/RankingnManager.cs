d1a87f60779de1ca34e16bf5cde7eb3222fa66a2
licrosea <sevenblock426@gmail.com>
2022-06-13 21:55:02 +0900

    余計なのも追跡しちゃってたので削除　ランキング・シングルトンのひな型を作成

---
+++ Assets/ThisProject/Scripts/GameSystem/RankingnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingnManager : SingletonMonoBehaviour<RankingnManager>
+{
+    /// <summary>
+    /// 各順位のデータ.
+    /// </summary>
+    public struct RankData
+    {
+        public string playerName; // プレイヤー名
+        public float penaltyTime; // ペナルティ時間
+        public int position; // 現在の順位
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// ランキングの初期化を行います.
+    /// </summary>
+    public void InitializeRanking()
+    {
+
+    }
+
+    /// <summary>
+    /// ランキングを更新します.
+    /// </summary>
+    public void UpdateRanking()
+    {
+
+    }
+}
