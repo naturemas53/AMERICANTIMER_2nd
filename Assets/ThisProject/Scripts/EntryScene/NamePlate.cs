0a88796b27e362f3e122a6d7790a072400e72feb
licrosea <sevenblock426@gmail.com>
2022-06-18 20:52:59 +0900

    ネームプレートのスクリプト追加　日本語フォント追加

---
+++ Assets/ThisProject/Scripts/EntryScene/NamePlate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NamePlate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject inputPanel;
+    [SerializeField]
+    GameObject displayPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputPanel.gameObject.SetActive(true);
+        displayPanel.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
