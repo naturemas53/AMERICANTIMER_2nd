2a458d7f6a08e4be6688594d0fa2c511a4df3494
licrosea <sevenblock426@gmail.com>
2022-07-09 22:44:16 +0900

    ラウンド設定での制限時間設定を実装

---
+++ Assets/ThisProject/Scripts/RoundSettingScene/TimeChangeCallBack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class TimeChangeCallBack : MonoBehaviour
+{
+    // なんかUnityEvent、継承しないと使えないっぽいので
+    // 取り急ぎ継承...
+    class FloatUnityEvent :  UnityEvent<float>{ }
+
+    [SerializeField]
+    float changeSecond = 0.0f;
+
+    UnityEvent<float> onPressedCallBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        onPressedCallBack = new FloatUnityEvent();
+
+        Button button = this.GetComponent<Button>();
+        button.onClick.AddListener(OnClickedButton);
+    }
+
+    void OnClickedButton()
+    {
+        onPressedCallBack.Invoke( changeSecond );
+    }
+
+    public void RegistCallBack( UnityAction<float> registCallBack )
+    {
+        onPressedCallBack.AddListener( registCallBack );
+    }
+}
---
+++ Assets/ThisProject/Scripts/RoundSettingScene/TimeSetting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeSetting : MonoBehaviour
+{
+    // 99分 ～ 1分まで
+    readonly float MAX_TIME = 60 * 99;
+    readonly float MIN_TIME = 60;
+
+    [SerializeField]
+    TextMeshProUGUI timerText;
+    [SerializeField]
+    List<TimeChangeCallBack> timeChangeButtons;
+
+    public float CurrentTime { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentTime = MIN_TIME;
+        UpdateTimeText();
+
+        foreach( var callBackButton in timeChangeButtons  )
+        {
+            callBackButton.RegistCallBack( OnPressedTimeChangeButton );
+        }
+    }
+
+    /// <summary>
+    /// 制限時間変動のボタンが押されたときのコールバック
+    /// </summary>
+    void OnPressedTimeChangeButton( float changeTime )
+    {
+        CurrentTime = Mathf.Clamp( CurrentTime + changeTime, MIN_TIME, MAX_TIME );
+        UpdateTimeText();
+    }
+
+    void UpdateTimeText()
+    {
+        timerText.text = TimeHelper.BuildTimeText( CurrentTime );
+    }
+}
