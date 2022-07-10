a923eed3fe2c2c86556e408d55d504d863f98bbc
licrosea <sevenblock426@gmail.com>
2022-07-10 22:39:23 +0900

    残り時間表示が意図したものとずれていたので修正

--- Assets/ThisProject/Scripts/TimerScene/TimerArea/RemainTimeView.cs
+++ Assets/ThisProject/Scripts/TimerScene/TimerArea/RemainTimeView.cs
@@ -21,6 +21,6 @@ public class RemainTimeView: MonoBehaviour
     {
         if (timeText == null || timer == null) return;
 
-        timeText.text = CommonHelper.BuildTimeText(timer.RemainTime);
+        timeText.text = CommonHelper.BuildTimeText(timer.RemainTime, true);
     }
 }
