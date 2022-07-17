da3af4fa5218263dcf8722e54b8ba52a0e4196d5
licrosea <sevenblock426@gmail.com>
2022-07-17 22:58:39 +0900

    設定した倍率を表示させる対応

---
+++ Assets/ThisProject/Scripts/TimerScene/TimerArea/MultiPlayerView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MultiPlayerView : MonoBehaviour
+{
+    [SerializeField]
+    DuelTimer duelTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( CoLateStart() );
+    }
+
+    IEnumerator CoLateStart()
+    {
+        yield return new WaitForEndOfFrame();
+
+        TextMeshProUGUI tproText = GetComponent<TextMeshProUGUI>();
+
+        if (tproText != null)
+        {
+            tproText.text = "x" + duelTimer.Multiplayer.ToString();
+        }
+    }
+}
